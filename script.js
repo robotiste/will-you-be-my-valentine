7a627fd872e5fc4653f6a11d6bfa1f27c16e7594
Vikas Kamboj <kambojvikas0043@gmail.com>
2026-02-14 16:24:42 +0530

    Add new messages to the confirmation prompts

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
+    "Aese mat kar Soumita",
     "Pookie please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
+    "Maan ja na Soumita",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
